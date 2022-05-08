566eb415081d385cf2e2db3bdc204652acc66552
garatina2202 <garatina2202@users.noreply.github.com>
2022-05-09 11:21:23 +1200

    added string splicing feature

---
+++ pig_latin_converter/pig_latin.py
@@ -0,0 +1,13 @@
+"""
+This script takes a word as input and uses indexing and 
+slicing to return its Pig Latin equivalent.
+"""
+
+while True:
+    word = input("type in an english word: ")
+    if (word[0] in {'a','e','i','o','u'}):
+        new_word = word + "way"
+    else:
+        new_word = word[1:] + word[0:1] + "ay"
+
+    
