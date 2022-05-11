7c7eff2bfc9b0da4aa576fb65c41dd5a4736b0a8
garatina2202 <garatina2202@users.noreply.github.com>
2022-05-12 10:06:16 +1200

    Defined main function and framework for functions

---
+++ Planet_Photo_Stacking/planet_stacking.py
@@ -0,0 +1,36 @@
+"""
+Photo Stacking Jupiter to form a clear image with less noise
+"""
+
+import os
+import re
+import sys
+import shutil
+from PIL import Image, ImageOps
+
+def del_folders(folder):
+    return
+
+def crop_images():
+    return
+
+def clean_folder():
+    return
+
+def main():
+    """ Get starting folder, copy folder, run crop function and clean folder."""
+
+    frames_folder = 'video_frames'
+
+    # preparing files and folders
+    del_folders('cropped')
+    shutil.copytree(frames_folder, 'cropped')
+
+    # run cropping function
+    print("Start cropping and scaling...")
+    os.chdir('cropped')
+    crop_images()
+    clean_folder()
+
+    print("Done \n")
+    
