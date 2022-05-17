4931a3d0b6f97a1743295031c658e991bf60953c
MossIV <garatina2202@users.noreply.github.com>
2022-05-17 14:34:39 +1200

    implemented cropping and scaling of images function

--- Planet_Photo_Stacking/planet_stacking.py
+++ Planet_Photo_Stacking/planet_stacking.py
@@ -19,6 +19,20 @@ def del_folders(folder):
     return
 
 def crop_images():
+    """
+    Crops and scale images of a planet to box around a planet
+    """
+    files = os.listdir()
+    for file_num, file in enumerate(files, start=1):
+        with Image.open(file) as img:
+            gray = img.convert('L')
+            bw = gray.point(lambda x: 0 if x< 90 else 255)
+            box = bw.getbbox()
+            padded_box = (box[0]-20,box[1]-20,box[2]+20,box[3]+20)
+            cropped = img.crop(padded_box)
+            scaled = ImageOps.fit(cropped,(860,860),Image.LANCZOS,0,(0.5,0.5))
+            file_name = f'cropped_{file_num}.jpg'
+            scaled.save(file_name, "JPEG")
     return
 
 def clean_folder(prefix_to_save):
