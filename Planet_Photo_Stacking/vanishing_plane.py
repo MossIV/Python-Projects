895242711f556cf8d060b5dc359743ee851d11f4
MossIV <garatina2202@users.noreply.github.com>
2022-05-18 14:29:16 +1200

    Fixed ln20-22 to properly remove plane

--- Planet_Photo_Stacking/vanishing_plane.py
+++ Planet_Photo_Stacking/vanishing_plane.py
@@ -12,14 +12,14 @@ green_data = []
 blue_data = []
 for image in images:
     with Image.open(image) as img:
-        if image == image[0]: # gets size of first cropped image
+        if image == images[0]: # gets size of first cropped image
             img_size = img.size
         red_data.append(list(img.getdata(0)))
         green_data.append(list(img.getdata(1)))
         blue_data.append(list(img.getdata(2)))
-med_red = [x[len(red_data/2)] for x in zip(*red_data)]
-med_green = [x[len(green_data)] for x in zip(*green_data)]
-med_blue = [x[len(blue_data)] for x in zip(*blue_data)]
+med_red = [sorted(x)[round(len(red_data)/2)] for x in zip(*red_data)]
+med_green = [sorted(x)[round(len(green_data)/2)] for x in zip(*green_data)]
+med_blue = [sorted(x)[round(len(blue_data)/2)] for x in zip(*blue_data)]
 
 merged_data = list(zip(med_red,med_green,med_blue))
 stacked = Image.new('RGB', (img_size))
